241524ff605f280dcc0d2bf0e4cd0b298fe411a0
elfaro1453 <54815985+elfaro1453@users.noreply.github.com>
2020-07-29 07:59:52 +0700

    Initial commit

---
+++ app/src/main/java/id/idn/fahru/covid19info/ChartCountryActivity.kt
@@ -0,0 +1,11 @@
+package id.idn.fahru.covid19info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartCountryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart_country)
+    }
+}
